26631e6156e4e0001032ce8ab818d6be1d77f2ae
Vaibhav <vaibhavbirlavb@gmail.com>
2019-12-26 01:37:38 +0530

    Json Parse Done

--- lib/stock_class.dart
+++ lib/stock_class.dart
@@ -1,44 +1,65 @@
+import 'dart:convert';
+
+Stock stockFromJson(String str) => Stock.fromJson(json.decode(str));
+
+String stockToJson(Stock data) => json.encode(data.toJson());
+
 class Stock {
-  String sYMBOL;
-  String nAMEOFCOMPANY;
-  String sERIES;
-  String dATEOFLISTING;
-  int pAIDUPVALUE;
-  int mARKETLOT;
-  String iSINNUMBER;
-  int fACEVALUE;
+    String symbol;
+    String nameOfCompany;
+    String series;
+    String dateOfListing;
+    int paidUpValue;
+    int marketLot;
+    String isinNumber;
+    int faceValue;
 
-  Stock(
-      {this.sYMBOL,
-      this.nAMEOFCOMPANY,
-      this.sERIES,
-      this.dATEOFLISTING,
-      this.pAIDUPVALUE,
-      this.mARKETLOT,
-      this.iSINNUMBER,
-      this.fACEVALUE});
+    Stock({
+        this.symbol,
+        this.nameOfCompany,
+        this.series,
+        this.dateOfListing,
+        this.paidUpValue,
+        this.marketLot,
+        this.isinNumber,
+        this.faceValue,
+    });
 
-  Stock.fromJson(Map<String, dynamic> json) {
-    sYMBOL = json['SYMBOL'];
-    nAMEOFCOMPANY = json['NAME OF COMPANY'];
-    sERIES = json['SERIES'];
-    dATEOFLISTING = json['DATE OF LISTING'];
-    pAIDUPVALUE = json['PAID UP VALUE'];
-    mARKETLOT = json['MARKET LOT'];
-    iSINNUMBER = json['ISIN NUMBER'];
-    fACEVALUE = json['FACE VALUE'];
-  }
+    factory Stock.fromJson(Map<String, dynamic> json) => Stock(
+        symbol: json["SYMBOL"],
+        nameOfCompany: json["NAME OF COMPANY"],
+        series: json["SERIES"],
+        dateOfListing: json["DATE OF LISTING"],
+        paidUpValue: json["PAID UP VALUE"],
+        marketLot: json["MARKET LOT"],
+        isinNumber: json["ISIN NUMBER"],
+        faceValue: json["FACE VALUE"],
+    );
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['SYMBOL'] = this.sYMBOL;
-    data['NAME OF COMPANY'] = this.nAMEOFCOMPANY;
-    data['SERIES'] = this.sERIES;
-    data['DATE OF LISTING'] = this.dATEOFLISTING;
-    data['PAID UP VALUE'] = this.pAIDUPVALUE;
-    data['MARKET LOT'] = this.mARKETLOT;
-    data['ISIN NUMBER'] = this.iSINNUMBER;
-    data['FACE VALUE'] = this.fACEVALUE;
-    return data;
-  }
+    get length => null;
+
+    Map<String, dynamic> toJson() => {
+        "SYMBOL": symbol,
+        "NAME OF COMPANY": nameOfCompany,
+        "SERIES": series,
+        "DATE OF LISTING": dateOfListing,
+        "PAID UP VALUE": paidUpValue,
+        "MARKET LOT": marketLot,
+        "ISIN NUMBER": isinNumber,
+        "FACE VALUE": faceValue,
+    };
+}
+
+
+class StockBundle{
+    final List<Stock> stocks;
+    StockBundle({this.stocks});
+
+    factory StockBundle.fromJson(List<dynamic> parsedJson){
+      List<Stock> stocks = new List<Stock>();
+
+      stocks = parsedJson.map((i) => Stock.fromJson(i)).toList();
+
+      return new StockBundle(stocks: stocks);
+    }
 }
